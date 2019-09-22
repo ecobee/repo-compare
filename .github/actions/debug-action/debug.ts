2549f274b4e4187c95d3a59aa450366e0d91c81a
Cory Taylor <cory.c.taylor@gmail.com>
2019-09-22 18:05:57 -0400

    fetch releases

--- .github/actions/debug-action/debug.ts
+++ .github/actions/debug-action/debug.ts
@@ -3,12 +3,25 @@ import * as github from '@actions/github'
 
 const run = async (): Promise<void> => {
     try {
+        console.log({ payload: github.context.payload })
+        
         const includePrerelease = core.getInput("include-prerelease");
         const message = `👋 Hello! I will include pre-releases ${includePrerelease}! 🙌`;
+
+        const token = process.env['GITHUB_TOKEN']
+        if (!token) return
+
+        const octokit: github.GitHub = new github.GitHub(token)
+        const nwo = process.env['GITHUB_REPOSITORY'] || '/'
+        const [owner, repo] = nwo.split('/')
+        const { data: releases } = await octokit.repos.listReleases({
+            owner,
+            repo
+        });
+        console.log(releases);
+        
         core.debug(message);
         core.setOutput("latest-release", message);
-        
-        console.log({payload: github.context.payload})
     } catch (error) {
         core.setFailed(`Debug-action failure: ${error}`);
     }
