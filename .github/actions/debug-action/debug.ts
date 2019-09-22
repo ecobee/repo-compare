1bd22192df91ff0004d7d6fd1c3057ffa8406c88
Cory Taylor <cory.c.taylor@gmail.com>
2019-09-22 19:02:32 -0400

    return unreleased commits

--- .github/actions/debug-action/debug.ts
+++ .github/actions/debug-action/debug.ts
@@ -4,10 +4,12 @@ import * as github from '@actions/github'
 const run = async (): Promise<void> => {
     try {
         console.log({ payload: github.context.payload })
-        
-        const includePrerelease = core.getInput("include-prerelease");
-        const message = `👋 Hello! I will include pre-releases ${includePrerelease}! 🙌`;
 
+        const includePrerelease: boolean = parseBoolean(
+            core.getInput('include-prerelease', {
+                required: false,
+            }),
+        )
         const token = process.env['GITHUB_TOKEN']
         if (!token) return
 
@@ -17,23 +19,29 @@ const run = async (): Promise<void> => {
         const { data: releases } = await octokit.repos.listReleases({
             owner,
             repo
-        });
+        })
         if (releases.length == 0) return
-        
+        const latestRelease = releases.find(element => element.prerelease = includePrerelease)
+        if (latestRelease == null) return
         const { data: comparison } = await octokit.repos.compareCommits({
             owner,
             repo,
-            base: releases[0].tag_name,
+            base: latestRelease.tag_name,
             head: 'master'
-        });
-        console.log(comparison);
-        core.debug(message);
-        core.setOutput("latest-release", message);
+        })
+        console.log(comparison.total_commits)
+        core.debug(`Master is ${comparison.status} by ${comparison.total_commits} commits`)
+        core.setOutput('unreleased-commits-count', comparison.total_commits.toString())
+        core.setOutput('unreleased-commits', comparison.commits.map(commit => commit.commit.message).toString())
     } catch (error) {
-        core.setFailed(`Debug-action failure: ${error}`);
+        core.setFailed(`Debug-action failure: ${error}`)
     }
 };
 
 run();
 
 export default run;
+
+function parseBoolean(toParse: string): boolean {
+    return !!(toParse && toParse.toLowerCase() == 'true');
+}
