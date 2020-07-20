468ad5fbbe644db396e4316cd2714b3416810091
Anton Nguyen <afnguyen85@gmail.com>
2020-07-20 07:44:00 -0400

    Fixed up the tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -3,6 +3,26 @@ import run from '../src/main'
 import fs from 'fs'
 import yaml from 'js-yaml'
 
+const nock = require('nock')
+const comparisonResponse = {
+  html_url: 'https://github.com/octocat/Hello-World/compare/master...topic',
+  status: 'behind',
+  ahead_by: 1,
+  behind_by: 2,
+  total_commits: 1,
+  commits: [
+    {
+      commit: {
+        message: 'Fix all the bugs'
+      },
+      author: {
+        login: 'octocat'
+      }
+    }
+  ]
+}
+const tagResponse = [{tag_name: 'v1.0.0', prerelease: false}]
+
 beforeEach(() => {
   jest.resetModules()
   const doc = yaml.safeLoad(
@@ -12,6 +32,16 @@ beforeEach(() => {
     const envVar = `INPUT_${name.replace(/ /g, '_').toUpperCase()}`
     process.env[envVar] = doc.inputs[name]['default']
   })
+  process.env.GITHUB_TOKEN = 'token'
+  process.env.GITHUB_REPOSITORY = 'foo/bar'
+  nock('https://api.github.com')
+    .persist()
+    .get('/repos/foo/bar/releases')
+    .reply(200, tagResponse)
+  nock('https://api.github.com')
+    .persist()
+    .get('/repos/foo/bar/compare/v1.0.0...master')
+    .reply(200, comparisonResponse)
 })
 
 afterEach(() => {
@@ -24,23 +54,39 @@ afterEach(() => {
   })
 })
 
-describe('debug action debug messages', () => {
-  it('outputs a debug message', async () => {
+describe('Run', () => {
+  it('calculates how many commits the repo is behind', async () => {
     const debugMock = jest.spyOn(core, 'debug')
     await run()
     expect(debugMock).toHaveBeenCalledWith(
-      '👋 Hello! I will include pre-releases false! 🙌'
+      'Master is behind by 1 commits'
     )
   })
-})
 
-describe('debug action output', () => {
-  it('sets the action output', async () => {
-    const setOutputMock = jest.spyOn(core, 'setOutput')
+  it('sets `unreleased-commit-count', async () => {
+    const setOutput = jest.spyOn(core, 'setOutput')
     await run()
-    expect(setOutputMock).toHaveBeenCalledWith(
-      'latest-release',
-      '👋 Hello! I will include pre-releases false! 🙌'
+    expect(setOutput).toHaveBeenCalledWith(
+      'unreleased-commit-count',
+      '1'
+    )
+  })
+
+  it('sets `unreleased-commit-messages', async () => {
+    const setOutput = jest.spyOn(core, 'setOutput')
+    await run()
+    expect(setOutput).toHaveBeenCalledWith(
+      'unreleased-commit-messages',
+      '@octocat - Fix all the bugs\n'
+    )
+  })
+
+  it('sets `unreleased-diff-url', async () => {
+    const setOutput = jest.spyOn(core, 'setOutput')
+    await run()
+    expect(setOutput).toHaveBeenCalledWith(
+      'unreleased-diff-url',
+      'https://github.com/octocat/Hello-World/compare/master...topic'
     )
   })
 })
